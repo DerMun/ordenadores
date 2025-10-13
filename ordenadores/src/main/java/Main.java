3c73f44841f034736cdfe7ca15e0a7220b8fca8c
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-10-13 20:44:03 -0300

    Update Main.java

--- ordenadores/src/main/java/Main.java
+++ ordenadores/src/main/java/Main.java
@@ -2,15 +2,34 @@ import utils.Sorters;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] vetor = {1, 22, 13, 44, 5, 0};
-        display(vetor);
+        if (args.length == 0) {
+            System.out.println("Erro! Falta de passagem de parâmetro ou parâmetro menor que 1!");
+            return;
+        }
 
-        Sorters.bubbleSort(vetor, 6); //forma de usar um utility
-        System.out.print("\n");
-
-        display(vetor);
+        int tamanho = Integer.parseInt(args[0]);
+        Integer[] array = gerarArrayAleatorio(tamanho);
+        
+        long inicio = System.nanoTime(); // marca tempo inicial
+        //Sorters.bubbleSort(array, tamanho);
+        Sorters.insertionSort(array, tamanho);
+        long fim = System.nanoTime();    // marca tempo final
+        double tempoMs = (fim - inicio)/1_000_000.0; // converte para milissegundos
+        
+        System.out.printf("\nTempo de execução: %.10f ms\n", tempoMs);
     }
 
+    static Integer[] gerarArrayAleatorio(int tamanho) {
+        Random random = new Random();
+        Integer[] array = new Integer[tamanho];
+
+        for (int i = 0; i < tamanho; i++) {
+            array[i] = random.nextInt(tamanho); // gera número de 0 a n
+        }
+
+        return array;
+    }
+    
     public static void display(Integer[] vetor){
         for (int i = 0; i < vetor.length; i++) {
             System.out.print(vetor[i]);
