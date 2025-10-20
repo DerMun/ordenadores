d593fb9c19e886d43da6cc9d9ae84ead185c1fef
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-10-20 09:55:10 -0300

    Atualizar o Main.java

--- ordenadores/src/main/java/Main.java
+++ ordenadores/src/main/java/Main.java
@@ -14,8 +14,6 @@ public class Main {
         
         op = vetor = algoritmo = n = 0;
 
-		do{
-
 		System.out.println("\n 1) Utilizar vetores de teste\n 2) Gerar vetor de tamanho personalizado\n 0) Sair da aplicacao");
 		System.out.print(" Escolha uma das opcoes: ");
 		op = input.nextInt();
@@ -36,6 +34,8 @@ public class Main {
 			System.exit(0);
 		}
 		
+do {
+
 		System.out.print(" Escolha o tipo de algoritmo: ");
 		System.out.println("\n 1) Bubble\n 2) Insertion\n 3) Selection\n 4) Shell\n 5) Heap\n 6) Merge\n 7) Quick\n 8) Couting\n 9) Radix\n");
 		algoritmo = input.nextInt();
