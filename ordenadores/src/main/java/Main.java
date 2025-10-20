553a9951ae76a4a5d128b260d23ae53e35b2ecef
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-10-20 13:59:01 -0300

    Update Main.java

--- ordenadores/src/main/java/Main.java
+++ ordenadores/src/main/java/Main.java
@@ -1,49 +1,57 @@
 import utils.Sorters;
 import utils.Vectors;
 import utils.ChooseSorter;
+import utils.ArrayGenerator;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         
-        int op,vetor,algoritmo,n;
+		int op,vetor,algoritmo,n;
         Integer[] array = null;
         
         Scanner input = new Scanner(System.in);
         
         op = vetor = algoritmo = n = 0;
 
-		System.out.println("\n 1) Utilizar vetores de teste\n 2) Gerar vetor de tamanho personalizado\n 0) Sair da aplicacao");
-		System.out.print(" \nEscolha uma das opcoes: ");
-		op = input.nextInt();
-		
-		if(op == 1){
-			System.out.println("\n 1) Ordem crescente\n 2) Ordem decrescente\n 3) Números aleatórios\n 4) Números repitidos");
-			System.out.print(" \nEscolha o tipo de vetor: ");
-			vetor = input.nextInt();
-			array = Vectors.escolheVetor(vetor);
-			n = 10;
-		} else if(op == 2) {
-			System.out.println("\n 1) Ordem crescente\n 2) Ordem decrescente\n 3) Números repitidos");
-			System.out.print(" \nEscolha o tipo de vetor: ");
-			vetor = input.nextInt();
-			System.out.print(" \nEscolha o tamanho do vetor: ");
-			n = input.nextInt();
-			array = Vectors.gerarArrayAleatorio(n);
-		} else {
-			System.out.print(" Opção inválida! ");
-			System.exit(0);
-		}
-		
 		do {
 
-		System.out.print(" Escolha o tipo de algoritmo: ");
-		System.out.println("\n 1) Bubble\n 2) Insertion\n 3) Selection\n 4) Shell\n 5) Heap\n 6) Merge\n 7) Quick\n 8) Couting\n 9) Radix\n");
-		algoritmo = input.nextInt();
+			System.out.println("\n 1) Utilizar vetores de teste\n 2) Gerar vetor de tamanho personalizado\n 0) Sair da aplicacao");
+			System.out.print(" Escolha uma das opcoes: ");
+			op = input.nextInt();
+		
+			switch(op) {
+				case 1:
+					System.out.println("\n 1) Ordem crescente\n 2) Ordem decrescente\n 3) Números aleatórios\n 4) Números repitidos");
+					System.out.print(" Escolha o tipo de vetor: ");
+					vetor = input.nextInt();
+					array = Vectors.escolheVetorTeste(vetor);
+					n = 10;
+					break;
+				case 2:
+					System.out.println("\n 1) Ordem crescente\n 2) Ordem decrescente\n 3) Números repitidos");
+					System.out.print(" Escolha o tipo de vetor: ");
+					vetor = input.nextInt();
+					System.out.print(" Escolha o tamanho do vetor: ");
+					n = input.nextInt();
+					array = Vectors.escolheVetorPersonalizado(vetor, n);
+					break;
+				case 0:
+					System.exit(0);
+			}
+			
+		
+			do {
+
+				System.out.println("\n 1) Bubble\n 2) Insertion\n 3) Selection\n 4) Shell\n 5) Heap\n 6) Merge\n 7) Quick\n 8) Couting\n 9) Radix\n 0) Sair");
+				System.out.print(" Escolha o tipo de algoritmo ou se deseja sair: ");
+				algoritmo = input.nextInt();
         
-		ChooseSorter.sorter(array, algoritmo, n);
-        
-		} while(op != 0); 
-    }
+				if(algoritmo > 0 & algoritmo <= 9){
+					ChooseSorter.sorter(array, algoritmo, n);
+				}
+			} while(algoritmo != 0);
+		} while(op != 0);
+	}
 }
