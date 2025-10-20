5bd38ca5bbf18c42c7bfdb919c565fe090a2a1ab
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-10-20 01:04:38 -0300

    Update Main.java

--- ordenadores/src/main/java/Main.java
+++ ordenadores/src/main/java/Main.java
@@ -1,20 +1,95 @@
 import utils.Sorters;
+import java.util.Random;
+import java.lang.System;
+import java.lang.String;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] vetor = {1, 22, 13, 44, 5, 0};
-        display(vetor);
+        if (args.length == 0) {
+            System.out.println("Erro! Falta de passagem de parâmetro ou parâmetro menor que 1!");
+            return;
+        }
+        
+        long inicio, fim;
+        inicio = fim = 0; 
 
-        Sorters.bubbleSort(vetor, 6); //forma de usar um utility
-        System.out.print("\n");
+        int tamanho = Integer.parseInt(args[0]);
+        String algoritmo = args[1];
+        
+        Integer[] array = gerarArrayAleatorio(tamanho);
+        
+        switch(algoritmo) {
+				case "bubble" :
+					display(array);
+					inicio = System.nanoTime(); // marca tempo inicial
+					Sorters.bubbleSort(array, tamanho);
+					fim = System.nanoTime();    // marca tempo final
+					display(array);
+					break;
+				case "insertion" :
+					display(array);
+					inicio = System.nanoTime();
+					Sorters.insertionSort(array, tamanho);
+					fim = System.nanoTime();
+					display(array);
+					break;
+				case "selection" :
+					display(array);
+					inicio = System.nanoTime();
+					Sorters.selectionSort(array, tamanho);
+					fim = System.nanoTime();
+					display(array);
+					break;
+				case "shell" :
+					display(array);
+					inicio = System.nanoTime();
+					Sorters.shellSort(array, tamanho);
+					fim = System.nanoTime();
+					display(array);
+				case "heap" :
+					display(array);
+					inicio = System.nanoTime();
+					Sorters.shellSort(array, tamanho);
+					fim = System.nanoTime();
+					display(array);
+					break;
+				case "merge" :
+					display(array);
+					inicio = System.nanoTime();
+					Sorters.mergeSort(array, 0, (tamanho - 1));
+					fim = System.nanoTime();
+					display(array);
+					break;
+				case "quick" :
+					display(array);
+					inicio = System.nanoTime();
+					Sorters.quickSort(array, 0, (tamanho - 1));
+					fim = System.nanoTime();
+					display(array);
+					break;
+		}
 
-        display(vetor);
+        double tempoMs = (fim - inicio)/1_000_000.0; // converte para milissegundos
+        
+        System.out.printf("\nTempo de execução: %.10f ms\n", tempoMs);
     }
 
-    public static void display(Integer[] vetor){
-        for (int i = 0; i < vetor.length; i++) {
-            System.out.print(vetor[i]);
+    static Integer[] gerarArrayAleatorio(int tamanho) {
+        Random random = new Random();
+        Integer[] array = new Integer[tamanho];
+
+        for (int i = 0; i < tamanho; i++) {
+            array[i] = random.nextInt(tamanho); // gera número de 0 a n
+        }
+
+        return array;
+    }
+    
+    public static void display(Integer[] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
             System.out.print(", ");
         }
+        System.out.print("\n");
     }
 }
