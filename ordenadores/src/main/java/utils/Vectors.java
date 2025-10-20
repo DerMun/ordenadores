76a0213255cfaeb0ad23c321b5799077dd2cf1b5
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-10-20 12:10:42 -0300

    Update Vectors.java

--- ordenadores/src/main/java/utils/Vectors.java
+++ ordenadores/src/main/java/utils/Vectors.java
@@ -4,10 +4,10 @@ import java.util.Random;
 public class Vectors{
 	static Integer[] vector_asc = {0,1,2,3,4,5,6,7,8,9};
 	static Integer[] vector_dec = {9,8,7,6,5,4,3,2,1,0};
-	static Integer[] vector_ale = gerarArrayAleatorio(10);
+	static Integer[] vector_ale = Array.generateRandomArray(10);
 	static Integer[] vector_rep = {0,5,7,3,9,2,9,3,1,0};
     
-    public static Integer[] escolheVetor(int vetor) {
+    public static Integer[] escolheVetorTeste(int vetor) {
 		switch(vetor) {
 			case 1:
 				return vector_asc;
@@ -21,17 +21,24 @@ public class Vectors{
 				return null;
 			}
 	}
-	
-	public static Integer[] gerarArrayAleatorio(int tamanho) {
-        Random random = new Random();
-        Integer[] array = new Integer[tamanho];
 
-        for (int i = 0; i < tamanho; i++) {
-            array[i] = random.nextInt(tamanho); // gera número de 0 a n
-        }
-
-        return array;
-    }
+	public static Integer[] escolheVetorPersonalizado(int vetor, int n) {
+		Integer[] array = null;
+		
+		switch(vetor) {
+			case 1:
+				array = Array.generateAscendingArray(n);
+				return array;
+			case 2:
+				array = Array.generateDescendingArray(n);
+				return array;
+			case 3:
+				array = Array.generateRepeatedArray(n);
+				return array;
+			default:
+				return null;
+			}
+	}
 	
 	public static void display(Integer[] array){
         for (int i = 0; i < array.length; i++) {
@@ -41,3 +48,4 @@ public class Vectors{
         System.out.print("\n");
     }
 }
+
